c680c055898b376caac94c741e69a8cfc15bbe88
NadetDev <nadetdev@gmail.com>
2025-03-27 21:16:33 +0100

    Add tailwingcss + 2 components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <app-header></app-header>
+    <h1>Welcome to {{ title }}!</h1>
 
     <router-outlet />
   `,
---
+++ src/app/components/primary-button/primary-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  imports: [],
+  template: ` <button (click)="handleButtonClick()">{{ label() }}</button> `,
+  styles: ``,
+})
+export class PrimaryButtonComponent {
+  label = input('');
+
+  handleButtonClick() {
+    console.log('handleButton clicked');
+  }
+}
